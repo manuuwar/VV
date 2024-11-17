7d80f52c92c8538887184998cdc04de616871e47
TheAdri666 <adrianinfante66@gmail.com>
2024-11-17 22:42:28 +0100

    Added more shot tests

--- Space-Invaders-Practica/src/test/java/main/ShotTest.java
+++ Space-Invaders-Practica/src/test/java/main/ShotTest.java
@@ -8,9 +8,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class ShotTest {
 
   @Test
-  void testShotInitialization() {
+  void testInitShotWithinBounds() {
     Shot shot = new Shot(100, 200);
     assertEquals(100 + 6, shot.getX()); // 100 + H_SPACE
     assertEquals(200 - 1, shot.getY()); // 200 - V_SPACE
+  }
+
+  @Test
+  void testInitShotOutOfBounds() {
+    Shot shot = new Shot(Commons.BOARD_WIDTH + 100, Commons.BOARD_HEIGHT + 200);
+    assertEquals(Commons.BOARD_WIDTH + 100 + 6, shot.getX());
+    assertEquals(Commons.BOARD_HEIGHT + 200 - 1, shot.getY());
+  }
+
+  @Test
+  void testInitShotNegativeCoordinates() {
+    Shot shot = new Shot(-100, -200);
+    assertEquals(-100 + 6, shot.getX());
+    assertEquals(-200 - 1, shot.getY());
   }
 }
